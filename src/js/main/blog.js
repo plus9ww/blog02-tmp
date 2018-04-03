d3b5514b0a5edfa3ec3d7f2be9437f6d9a855832
plus9ww <info@plus9ww.jp>
2018-04-03 16:11:11 +0900

    Initial Commit

--- src/js/main/blog.js
+++ src/js/main/blog.js
@@ -1,4 +1,6 @@
 ﻿$(window).load(function () {
+	
+	if(windowsize()) {
  
       //該当のセレクタなどを代入
        
@@ -7,64 +9,71 @@
       var sideArea = $("#js__sideMain"); //サイドバー
  
       /*設定ここまで*/
-       
-      var wd = $(window); //ウィンドウ自体
-       
-       
-      //メインとサイドの高さを比べる
-       
-      var mainH = mainArea.height();
-      var sideH = sideWrap.height();
-       
-       
-      if(sideH < mainH) { //メインの方が高ければ色々処理する
-             
-            //サイドバーの外枠をメインと同じ高さにしてrelaltiveに（#sideをポジションで上や下に固定するため）
-            sideWrap.css({"height": mainH,"position": "relative"});
-       
-            //サイドバーがウィンドウよりいくらはみ出してるか
-            var sideOver = wd.height()-sideArea.height();
-       
-            //固定を開始する位置 = サイドバーの座標＋はみ出す距離
-            var starPoint = sideArea.offset().top + (-sideOver);
-             
-            //固定を解除する位置 = メインコンテンツの終点
-            var breakPoint = sideArea.offset().top + mainH;
-       
-            wd.scroll(function() { //スクロール中の処理
-                   
-                  if(wd.height() < sideArea.height()){ //サイドメニューが画面より大きい場合
-                        if(starPoint < wd.scrollTop() && wd.scrollTop() + wd.height() < breakPoint){ //固定範囲内
-                              sideArea.css({"position": "fixed", "bottom": "20px"}); 
-       
-                        }else if(wd.scrollTop() + wd.height() >= breakPoint){ //固定解除位置を超えた時
-                              sideArea.css({"position": "absolute", "bottom": "0"});
-       
-                        } else { //その他、上に戻った時
-                              sideArea.css("position", "static");
-       
-                        }
-       
-                  }else{ //サイドメニューが画面より小さい場合
-                   
-                        var sideBtm = wd.scrollTop() + sideArea.height(); //サイドメニューの終点
-                         
-                        if(mainArea.offset().top < wd.scrollTop() && sideBtm < breakPoint){ //固定範囲内
-                              sideArea.css({"position": "fixed", "top": "20px"});
-                               
-                        }else if(sideBtm >= breakPoint){ //固定解除位置を超えた時
-                         
-                              //サイドバー固定場所（bottom指定すると不具合が出るのでtopからの固定位置を算出する）
-                              var fixedSide = mainH - sideH;
-                               
-                              sideArea.css({"position": "absolute", "top": fixedSide});
-                               
-                        } else {
-                              sideArea.css("position", "static");
-                        }
-                  }
-            });
-      }
+		   
+		  var wd = $(window); //ウィンドウ自体
+		   
+		   
+		  //メインとサイドの高さを比べる
+		   
+		  var mainH = mainArea.height();
+		  var sideH = sideWrap.height();
+		   
+		   
+		  if(sideH < mainH) { //メインの方が高ければ色々処理する
+				 
+				//サイドバーの外枠をメインと同じ高さにしてrelaltiveに（#sideをポジションで上や下に固定するため）
+				sideWrap.css({"height": mainH,"position": "relative"});
+		   
+				//サイドバーがウィンドウよりいくらはみ出してるか
+				var sideOver = wd.height()-sideArea.height();
+		   
+				//固定を開始する位置 = サイドバーの座標＋はみ出す距離
+				var starPoint = sideArea.offset().top + (-sideOver);
+				 
+				//固定を解除する位置 = メインコンテンツの終点
+				var breakPoint = sideArea.offset().top + mainH;
+		   
+				wd.scroll(function() { //スクロール中の処理
+				
+					if(windowsize()) {
+					   
+					  if(wd.height() < sideArea.height()){ //サイドメニューが画面より大きい場合
+							if(starPoint < wd.scrollTop() && wd.scrollTop() + wd.height() < breakPoint){ //固定範囲内
+								  sideArea.css({"position": "fixed", "bottom": "20px"}); 
+		   
+							}else if(wd.scrollTop() + wd.height() >= breakPoint){ //固定解除位置を超えた時
+								  sideArea.css({"position": "absolute", "bottom": "0"});
+		   
+							} else { //その他、上に戻った時
+								  sideArea.css("position", "static");
+		   
+							}
+		   
+					  }else{ //サイドメニューが画面より小さい場合
+					   
+							var sideBtm = wd.scrollTop() + sideArea.height(); //サイドメニューの終点
+							 
+							if(mainArea.offset().top < wd.scrollTop() && sideBtm < breakPoint){ //固定範囲内
+								  sideArea.css({"position": "fixed", "top": "20px"});
+								   
+							}else if(sideBtm >= breakPoint){ //固定解除位置を超えた時
+							 
+								  //サイドバー固定場所（bottom指定すると不具合が出るのでtopからの固定位置を算出する）
+								  var fixedSide = mainH - sideH;
+								   
+								  sideArea.css({"position": "absolute", "top": fixedSide});
+								   
+							} else {
+								  sideArea.css("position", "static");
+							}
+					  }
+					}else{
+						 sideArea.css("position", "static");
+						 sideWrap.css({"height": sideH});
+					}
+				});
+		  }
+	  }
 });
 
 $(function () {
